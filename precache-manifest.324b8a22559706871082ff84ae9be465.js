2266d19187c2cc87fa2475604ac2d0cac5351304
Lý Đông Cảnh <lydongcanh@gmail.com>
2020-10-10 01:27:03 +0700

    Updates

---
+++ precache-manifest.324b8a22559706871082ff84ae9be465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a46f6cddb94ad9c635e48fb9f4c390ae",
+    "url": "/aic/index.html"
+  },
+  {
+    "revision": "442399c0e9346760cec6",
+    "url": "/aic/static/js/2.7080a5a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aic/static/js/2.7080a5a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e64ce59b393dd88f3a3",
+    "url": "/aic/static/js/main.b3f9d3f5.chunk.js"
+  },
+  {
+    "revision": "d079f113830128eae63c",
+    "url": "/aic/static/js/runtime-main.a5d4b7dc.js"
+  }
+]);
